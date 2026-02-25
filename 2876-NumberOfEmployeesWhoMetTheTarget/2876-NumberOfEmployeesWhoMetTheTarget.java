134a97863358459bc9089030c40e8822d12129c0
Harsanth0430 <harsanthharsanth5@gmail.com>
2026-02-25 11:19:06 +0530

    Create 2876-NumberOfEmployeesWhoMetTheTarget

---
+++ 2876-NumberOfEmployeesWhoMetTheTarget/2876-NumberOfEmployeesWhoMetTheTarget.java
@@ -0,0 +1,12 @@
+// Last updated: 25/02/2026, 11:19:10
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count=0;
+        for(int i=0;i<hours.length;i++){
+            if(hours[i]>=target){
+                count++;
+            }
+        }
+        return count;
+    }
+}
