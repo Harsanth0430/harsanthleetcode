cc3d2294592fbab407e85292b5685426f6e562fa
Harsanth0430 <harsanthharsanth5@gmail.com>
2026-03-12 22:06:04 +0530

    File updated at 12/03/2026, 22:06:11

--- 75-SortColors/75-SortColors.java
+++ 75-SortColors/75-SortColors.java
@@ -1,18 +1,15 @@
-// Last updated: 25/02/2026, 11:20:51
-class Solution {
-    public void sortColors(int[] nums) {
-        int n=nums.length;
-        int one=0;
-        int two=0;
-        int zero=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0) zero++;
-            if(nums[i]==1) one++;
-            if(nums[i]==2) two++;
-        }
-        int index=0;
-        for(int i=0;i<zero;i++) nums[index++]=0;
-        for(int i=0;i<one;i++) nums[index++]=1;
-        for(int i=0;i<two;i++) nums[index++]=2;
-    }
-}
+// Last updated: 12/03/2026, 22:06:11
+1class Solution {
+2    public void sortColors(int[] nums) {
+3       for(int i=0;i<nums.length;i++){
+4        for(int j=0;j<nums.length-1;j++){
+5        if(nums[j]>nums[i]){
+6            int temp=nums[i];
+7           nums[i]=nums[j];
+8           nums[j]=temp;
+9        }
+10    }
+11    }
+12    return ;
+13}
+14}
